f8e6b5f2b05afde5e549239dc209284747cbc2bd
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-17 22:44:40 -0400

    refactored so app will only start when mongo db connection is successful

--- app.js
+++ app.js
@@ -18,11 +18,10 @@ mongoose
 	})
 	.then(() => {
 		logger.info('Connected to MongoDB')
+		app.listen(config.PORT, () => {
+			logger.info(`App running on port ${config.PORT}`)
+		})
 	})
 	.catch((error) => {
 		logger.error('Error connecting to MongoDB:', error.message)
 	})
-
-app.listen(config.PORT, () => {
-	logger.info(`App running on port ${config.PORT}`)
-})
