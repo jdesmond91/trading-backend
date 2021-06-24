1964d325351bebf0c31c72cadbee159ad7d613fe
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-23 21:50:06 -0400

    created security controller with get and post methods

---
+++ controllers/securities.js
@@ -0,0 +1,39 @@
+const Security = require('../models/security')
+const securitiesRouter = require('express').Router()
+const logger = require('../utils/logger')
+
+securitiesRouter.get('/', async (req, res) => {
+	try {
+		const securities = await Security.find({})
+		res.status(200).json(securities)
+	} catch (err) {
+		const message = 'Could not retreive securities from database'
+		logger.error({
+			message: message,
+			error: err,
+		})
+		res.status(400).json(message)
+	}
+})
+
+securitiesRouter.post('/', async (req, res) => {
+	const newSecurity = new Security({
+		name: req.body.name,
+		ticker: req.body.ticker,
+		price: req.body.price,
+	})
+
+	try {
+		await newSecurity.save()
+		res.status(201).json(newSecurity)
+	} catch (err) {
+		const message = 'Could not save new security to database'
+		logger.error({
+			message: message,
+			error: err,
+		})
+		res.status(400).json(message)
+	}
+})
+
+module.exports = securitiesRouter
