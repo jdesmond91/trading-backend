47992ad6c11dd47ed18cea1d61f6d9246e800946
jdesmond91 <jonathandesmond91@gmail.com>
2021-06-29 20:12:25 -0400

    added models for transactions and positions

---
+++ models/position.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const positionSchema = new mongoose.Schema({
+	security: { type: mongoose.Schema.Types.ObjectId, ref: 'Security', required: true },
+	quantity: { type: Number, required: true },
+})
+
+// when toJSON is called, delete mongoose id and version and return id as string
+positionSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
+
+module.exports = mongoose.model('Position', positionSchema)
---
+++ models/transaction.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const transactionSchema = new mongoose.Schema({
+	type: { type: String, require: true },
+	date: { type: Date, required: true },
+	amount: { type: Number, required: true },
+	order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+})
+
+// when toJSON is called, delete mongoose id and version and return id as string
+transactionSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
+
+module.exports = mongoose.model('Transaction', transactionSchema)
