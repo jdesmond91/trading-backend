7adb7474f3c96ad557237954522cb4d483a936ac
jdesmond91 <jonathandesmond91@gmail.com>
2021-07-03 13:44:28 -0400

    added get cash position route

--- controllers/positions.js
+++ controllers/positions.js
@@ -1,6 +1,7 @@
 const Position = require('../models/position')
 const positionsRouter = require('express').Router()
 const logger = require('../utils/logger')
+const { getCashPosition } = require('./helpers')
 
 positionsRouter.get('/', async (req, res) => {
 	try {
@@ -8,6 +9,20 @@ positionsRouter.get('/', async (req, res) => {
 		res.status(200).json(positions)
 	} catch (err) {
 		const message = 'Could not retrieve positions from database'
+		logger.error({
+			message: message,
+			error: err,
+		})
+		res.status(400).json(message)
+	}
+})
+
+positionsRouter.get('/cash', async (req, res) => {
+	try {
+		const cash = await getCashPosition()
+		res.status(200).json(cash)
+	} catch (err) {
+		const message = 'Could not retrieve cash position from database'
 		logger.error({
 			message: message,
 			error: err,
