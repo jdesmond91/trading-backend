561038b9fe4d1a7e39f1d60beadaf3667a4087c6
jdesmond91 <jonathandesmond91@gmail.com>
2021-08-09 14:06:38 -0400

    added export for REDIS_PORT

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 
 let MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
+const REDIS_PORT = process.env.REDIS_PORT
 
 if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
 	MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
-module.exports = { MONGODB_URI, PORT }
+module.exports = { MONGODB_URI, PORT, REDIS_PORT }
